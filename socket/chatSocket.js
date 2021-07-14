cd04249e5a96407385bac94f1a0330808d0ce69f
Mansoor shakir <mansoorshakir642@gmail.com>
2021-07-14 15:32:03 +0530

    done

--- socket/chatSocket.js
+++ socket/chatSocket.js
@@ -1,6 +1,8 @@
+const fs=require("fs");
+
 const chatSocket = (socket, io) => {
   socket.on("message", (payload) => {
-    socket.emit("reply", { message: "Bhag bhenke ke ghode" });
+    console.log(paylaod)
   });
 };
 
